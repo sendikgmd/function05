aef2401cffbb3b09a74a0fd4e4c54a5bb80b8b02
sendikgmd <163888441+sendikgmd@users.noreply.github.com>
2024-06-13 11:36:34 +0300

    Добавьте файлы проекта.

---
+++ function06/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            ShuffleArray(array);
+            Console.ReadKey();
+        }
+
+        static void ShuffleArray(int[] array)
+        {
+            Random random = new Random();
+            int randomIndex;
+            int shuffledElement;
+
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                randomIndex = random.Next(i);
+                shuffledElement = array[randomIndex];
+                array[randomIndex] = array[i];
+                array[i] = shuffledElement;
+                Console.Write(shuffledElement + " ");
+            }
+        }
+    }
+}
